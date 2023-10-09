8662b0ff17a83c9ee718bc68c1910c7821ffcbda
Vladimir Zaikin <friend_zva@yandex.ru>
2023-10-09 15:45:13 +0300

    TRYYYYYYY

--- src/protocol.c
+++ src/protocol.c
@@ -1,40 +1,53 @@
 #include <protocol.h>
 
 int read_message(FILE *stream, void *buf) {
-    int count_correct_read_number;
-    buf[count_correct_read_number++] = 0x7E;
-    int mask = 0b11111;
-    int number_read;
-    int count_shift;
-    u_int8_t number_tmp;
-
-    while ((number_read = getc(stream)) != EOF) {
-        int count_cycle = 0;
-        number_tmp = number_read;
-        number_tmp >>= count_shift;
-        u_int8_t number_correct = count_one(number_read);
-        do {
-            count_cycle++;
-            if (number_tmp == mask) {
-                number_tmp <<= 1;
-                count_shift++;
-                break;
-            }    
-            number_tmp >>= 1;
-
-        } while (number_tmp != mask && count_cycle < 3);
-        buf[count_correct_read_number++] = number_read >> count_shift;
-    buf[count_correct_read_number] = 0x7E;
-    return count_correct_read_number;
-}
-
-int write_message(FILE *stream, void *buf, size_t nbyte) {
+    u_int8_t *x = (u_int8_t *) buf;
     int count_correct_write_number;
 
-    for (int i = 0; i < nbyte; ++i) {
-        putc(buf[i], stream);
+    for (int i = 0; i < 10; ++i) {
+        putc(x[i], stream);
         count_correct_write_number++;
     }
 
+    return count_correct_write_number;
+}
+
+int write_message(FILE *stream, const void *buf, size_t nbyte) {
+    u_int8_t *x = (u_int8_t *) buf; // только новую переменную создавать?
+    putc(0x77, stream); //проверка ?
+    int mask = 0b11111;
+    int count_correct_write_number = 1;
+    int count_shift = 0;
+    u_int8_t number_tmp = 0;
+    u_int8_t number_correct = 0;
+    int count_cycle = 0;
+
+    for (int i = 0; i < nbyte; ++i) {
+        number_tmp = (x[i] >> count_shift) | number_correct; // проверка?
+        
+        for (int i = 3; i > -1; i--) {
+            number_tmp >>= i;
+            if (number_tmp == mask) {
+                count_shift++;
+                count_cycle = i;
+                break;
+            }
+        }
+
+        //number_correct = (buf[i] << (8 - count_cycle));
+        //printf("%x\n", number_tmp); ???????
+        putc((number_tmp << count_cycle) | ((x[i] << (8 - count_cycle)) >> (8 - count_cycle + 1)), stream);
+        count_correct_write_number++;
+        count_cycle = 0;
+
+        number_correct = (x[i] << (8 - count_cycle)) >> (8 - count_cycle + 1); // если 0, то все равно сдвинется
+    }
+    if (count_shift != 0) {
+        putc((0x77 >> count_shift) | number_correct, stream);
+        count_correct_write_number++;
+        putc((0x77 << (8 - count_shift)) | (0xFF >> count_shift), stream);
+    } else {
+        putc(0x77, stream);
+    }
     return count_correct_write_number;
 }
