6be26fedc760589d6d1b8b9ce3745d92601cd5c1
Vladimir Zaikin <friend_zva@yandex.ru>
2023-10-31 19:26:13 +0300

    halava

---
+++ build/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/vladimir/myProject/myTests/testGit/test/src/sort.s" "/Users/vladimir/myProject/myTests/testGit/test/build/src/CMakeFiles/main.dir/sort.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vladimir/myProject/myTests/testGit/test/src/main.c" "src/CMakeFiles/main.dir/main.c.o" "gcc" "src/CMakeFiles/main.dir/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
