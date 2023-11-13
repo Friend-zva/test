38b33931e6b5b7c4bc9cba6d7ea3285b37d96257
Vladimir Zaikin <friend_zva@yandex.ru>
2023-11-13 21:50:44 +0300

    New task

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@
 
 BASEDIR=$(realpath "$(dirname "$0")")
 ROOTDIR=$(realpath "$BASEDIR/..")
+BUILDDIR=$(realpath "$ROOTDIR/build")
 
-cmake -S . -B "$ROOTDIR/build"
-cmake --build "$ROOTDIR/build"
+cmake -S . -B "$BUILDDIR"
+cmake --build "$BUILDDIR"
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,11 @@
 
 BASEDIR=$(realpath "$(dirname "$0")")
 ROOTDIR=$(realpath "$BASEDIR/..")
+MAIN=$(realpath "$ROOTDIR/build/main")
+TESTDIR=$(realpath "$ROOTDIR/test")
 
-cd "$ROOTDIR/build"
-ctest --verbose
+printf "\033[0;32m%s\033[0m\n" "Normal input"
+"$MAIN" < "$TESTDIR/normal_input.txt"
+
+printf "\033[0;32m%s\033[0m\n" "Hacker input"
+"$MAIN" < "$TESTDIR/hacker_input.txt" 2>&1 >/dev/null | grep other
